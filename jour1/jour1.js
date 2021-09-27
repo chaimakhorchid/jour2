34418d50ea46518ce94259675fcc94034005042f
chaimakhorchid <khorchid.chaima@hotmail.fr>
2021-09-27 18:51:11 +0200

    modif

---
+++ jour1/jour1.js
@@ -0,0 +1,29 @@
+console.log ("Hello World !")
+
+var test = "My name is Chaïma"
+console.log(test)
+
+
+var Name = "Chaïma"
+console.log( "Nice to meet you " + Name)
+
+
+var testLength = "I'm very long !"
+console.log(testLength.length)
+
+var food = "croissant is meh" 
+food = food.replace ("meh", "so good")
+console.log(food)
+
+var basic = "This is cool "
+var basicUp = basic.toLocaleUpperCase()
+var basicDown = basic.toLowerCase()
+console.log(basic+ basicUp + basicDown)
+
+var world = "banana"
+var letter = world.split("")
+console.log(letter)
+
+var age = 25
+var template = "I'm  years old"
+console.log(`I'm ${age} years old `);
