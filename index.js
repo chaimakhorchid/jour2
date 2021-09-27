234c53a897229898b68faf7f25ef9fedc54489ab
chaimakhorchid <khorchid.chaima@hotmail.fr>
2021-09-27 16:32:44 +0200

    modif

---
+++ index.js
@@ -0,0 +1,64 @@
+var integer = 102
+var float = 13.9
+console.log(integer)
+
+
+var basic = 34
+console.log(basic)
+console.log(basic.length)
+
+basic = basic.toString()
+console.log(basic)
+
+
+var num = 1.5
+Math.round(1.5)
+var rounded = 1.5
+console.log(rounded)
+
+var test = 12
+var bis = 5
+console.log(test+bis)
+console.log(test-bis)
+console.log(test*bis)
+console.log(test/bis)
+console.log(test**bis)
+console.log(test%bis)
+
+
+var test = 143
+var bis = 219
+
+console.log(test<bis)
+console.log(test>bis)
+console.log(test<=bis)
+console.log(test>=bis)
+console.log(test==bis)
+console.log(test===bis)
+console.log(test!=bis)
+console.log(test!==bis)
+
+
+var limit = 50
+var score = 64
+
+if(score>=limit){
+console.log("ok!Good")}
+else{
+    console.log("oh!Noooooo...")
+}
+
+var password = "azerty"
+if(password.length > 5){
+    console.log("The password is secure")
+}
+
+
+if (score >= limit && password.length > 5) {
+	console.log("everything is good");
+}
+if (score >= limit || password.length > 5) {
+	console.log("something is good");
+} else {
+	console.log("nothing is good");
+}
